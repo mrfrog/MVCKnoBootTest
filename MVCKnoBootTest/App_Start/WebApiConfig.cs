8290aa0cb0ec8cf91606d74a183518ea1662d521
mrfrog <gianluca.limatola@gmail.com>
2014-07-17 10:28:59 +0200

    create cartelle Api e Mvc in Controller

--- MVCKnoBootTest/App_Start/WebApiConfig.cs
+++ MVCKnoBootTest/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace MVCKnoBootTest
@@ -9,6 +10,8 @@ namespace MVCKnoBootTest
     {
         public static void Register(HttpConfiguration config)
         {
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
